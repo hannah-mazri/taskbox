ff50dfc3ed24368dc3e8a0198db454e7ec66fe3a
hannah <hannahnajwa@gmail.com>
2021-08-15 16:41:33 +0800

    build out states in TaskList component

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -5,26 +5,43 @@ import { Task } from '../models/task.model';
   selector: 'app-task-list',
   template: `
     <div class="list-items">
-      <div *ngIf="loading">loading</div>
-      <div *ngIf="!loading && tasks.length === 0">empty</div>
       <app-task
-        *ngFor="let task of tasks"
+        *ngFor="let task of tasksInOrder"
         [task]="task"
         (onArchiveTask)="onArchiveTask.emit($event)"
         (onPinTask)="onPinTask.emit($event)"
       >
       </app-task>
+      <div *ngIf="tasksInOrder.length == 0 && !loading" class="wrapper-message">
+        <span class="icon-check"></span>
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
+      <div *ngIf="loading">
+        <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
+          <span class="glow-checkbox"></span>
+          <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
+        </div>
+      </div>
     </div>
   `
 }))
 export class TaskListComponent {
-  @Input() tasks: Task[] = [];
+  @Input() tasksInOrder: Task[] = [];
 
   @Input() loading = false;
 
   @Output()
-  onPinTask = new EventEmitter<Event>();
+  onPinTask: EventEmitter<any> = new EventEmitter();
 
   @Output()
-  onArchiveTask = new EventEmitter<Event>();
+  onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  @Input()
+  set tasks(arr: Task[]) {
+    this.tasksInOrder = [
+      ...arr.filter(t => t.state === 'TASK_PINNED'),
+      ...arr.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+  }
 }
