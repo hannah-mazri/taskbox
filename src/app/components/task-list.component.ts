9ab8e0cb199f3293baa7affe79382aa1266f0cd6
hannah <hannahnajwa@gmail.com>
2021-08-16 14:44:25 +0800

    separate PureTaskList and TaskList components

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,47 +1,32 @@
-import {Input, Output, EventEmitter, Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { ArchiveTask, PinTask, TasksState } from '../state/task.state';
 import { Task } from '../models/task.model';
+import { Observable } from 'rxjs';
 
 @Component(({
   selector: 'app-task-list',
   template: `
     <div class="list-items">
-      <app-task
-        *ngFor="let task of tasksInOrder"
-        [task]="task"
-        (onArchiveTask)="onArchiveTask.emit($event)"
-        (onPinTask)="onPinTask.emit($event)"
+      <app-pure-task-list
+        [tasks]="tasks$ | async"
+        (onArchiveTask)="archiveTask($event)"
+        (onPinTask)="pinTask($event)"
       >
-      </app-task>
-      <div *ngIf="tasksInOrder.length == 0 && !loading" class="wrapper-message">
-        <span class="icon-check"></span>
-        <div class="title-message">You have no tasks</div>
-        <div class="subtitle-message">Sit back and relax</div>
-      </div>
-      <div *ngIf="loading">
-        <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
-          <span class="glow-checkbox"></span>
-          <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
-        </div>
-      </div>
+      </app-pure-task-list>
     </div>
   `
 }))
 export class TaskListComponent {
-  @Input() tasksInOrder: Task[] = [];
+  @Select(TasksState.getAllTasks) tasks$: Observable<Task[]>;
 
-  @Input() loading = false;
+  constructor(private store: Store) {}
 
-  @Output()
-  onPinTask: EventEmitter<any> = new EventEmitter();
+  archiveTask(id: string): void {
+    this.store.dispatch(new ArchiveTask(id));
+  }
 
-  @Output()
-  onArchiveTask: EventEmitter<any> = new EventEmitter();
-
-  @Input()
-  set tasks(arr: Task[]) {
-    this.tasksInOrder = [
-      ...arr.filter(t => t.state === 'TASK_PINNED'),
-      ...arr.filter(t => t.state !== 'TASK_PINNED'),
-    ];
+  pinTask(id: string): void {
+    this.store.dispatch(new PinTask(id));
   }
 }
